9d91c75414ec9fa4682b80cb1366fa06fdea46f0
matkrin <matkrin@protonmail.com>
2021-09-04 12:39:44 +0200

    split main html template in base_template.jinja and seperate templates for each class type

--- promulstm/html_rendering.py
+++ promulstm/html_rendering.py
@@ -3,10 +3,10 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def create_html(class_obj_lst, output_path):
-    """
-    class_obj_lst: list with all class objects for the html report
+    """class_obj_lst: list with all class objects for the html report
     output_name: name or full path of the html report
     """
+
 
     def check_type(class_obj, check_str):
         """helper function as jinja does not support much python logic"""
@@ -15,41 +15,18 @@ def create_html(class_obj_lst, output_path):
 
 
     output_name = os.path.basename(output_path)
-    template_dir = os.path.join(os.path.dirname(__file__), 'template')
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 
     env = Environment(loader=FileSystemLoader(template_dir))
     env.globals['check_type'] = check_type      #adds function to jinja env
 
-    template = env.get_template('template.html')
+    template = env.get_template('base_template.jinja')
 
-    with open(f'{template_dir}/style.css') as f:
-        style_css = f.read()
-
-    with open(f'{template_dir}/slideshow.js') as f:
-        slideshow = f.read()
-
-    with open(f'{template_dir}/bokeh-2.3.1.min.js') as f:
-        bokeh_231 = f.read()
-
-    with open(f'{template_dir}/bokeh-widgets-2.3.1.min.js') as f:
-        bokeh_231_widgets = f.read()
-
-    with open(f'{template_dir}/bokeh-tables-2.3.1.min.js') as f:
-        bokeh_231_tables = f.read()
-
-    with open(f'{template_dir}/top_button.js') as f:
-        top_button = f.read()
 
     output = template.render(
         cls_objs = class_obj_lst,
         title = output_name,
         files_dir = output_path,
-        style_css = style_css,
-        slideshow = slideshow,
-        bokeh_231 = bokeh_231,
-        bokeh_231_widgets = bokeh_231_widgets,
-        bokeh_231_tables = bokeh_231_tables,
-        top_button = top_button,
     )
 
     with open(f'{output_path}_report.html', 'w') as f:
