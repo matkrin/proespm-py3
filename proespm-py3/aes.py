9ebd16755b0faf9527f022c7adf5fd59ef4d15dd
matkrin <matkrin@protonmail.com>
2022-08-07 18:33:34 +0200

    Changed np.arange to np.linspace in read_vms (+-1 datapoint bug)

--- proespm-py3/aes.py
+++ proespm-py3/aes.py
@@ -58,12 +58,17 @@ class Aes:
             elif i.label == "BKSresol":
                 self.res = i.value
 
-        x_values = np.arange(
+        x_values = np.linspace(
             data.x_start,
-            data.x_step * data.num_y_values + data.x_start,
-            data.x_step,
+            (data.x_step * data.num_y_values + data.x_start)-data.x_step,
+            num=data.num_y_values,
         )
         y_values = np.array(data.corresponding_variables[0].y_values)
+        print("num_y_values", data.num_y_values)
+        print("len y", len(y_values))
+        print("len x", len(x_values))
+        print("start x", data.x_start)
+        print("step x", data.x_step)
         self.aes_data = np.column_stack((x_values, y_values))
 
     def read_staib_dat(self, filepath: str) -> None:
