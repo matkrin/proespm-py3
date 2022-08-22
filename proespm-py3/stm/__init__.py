8f37bccbebcc1dcd2133aa38810388f18a0534e0
matkrin <matkrin@protonmail.com>
2022-08-22 20:39:55 +0200

    Matrix no traces bug - HTML report shows files which could not be processed

--- proespm-py3/stm/__init__.py
+++ proespm-py3/stm/__init__.py
@@ -1,8 +1,22 @@
+import os
+import datetime
+
 from .stm_mul import StmMul
 from .stm_flm import StmFlm
 from .stm_sm4 import StmSm4
 from .stm_matrix import NoTracesError, StmMatrix
 from .stm_sxm import StmSxm
+
+
+class ErrorFile:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.basename = os.path.basename(self.filepath)
+        self.dirname = os.path.dirname(self.filepath)
+        self.filename, self.fileext = os.path.splitext(self.basename)
+        self.datetime = datetime.datetime.utcfromtimestamp(
+            os.path.getmtime(filepath)
+        )
 
 
 def stm_factory(file):
@@ -15,7 +29,7 @@ def stm_factory(file):
             return StmMatrix(file)
         except NoTracesError as e:
             print("An error occurred: ", e)
-            return None
+            return ErrorFile(file)
     elif file.endswith(".SM4"):
         return StmSm4(file)
     elif file.endswith(".sxm"):
