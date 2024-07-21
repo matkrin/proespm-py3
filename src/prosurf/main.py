c2b73cf55f61e43f64acdb67242734f55ab12e24
matkrin <matkrin@protonmail.com>
2024-07-21 09:48:56 +0200

    Clean up main

--- src/prosurf/main.py
+++ src/prosurf/main.py
@@ -1,44 +1,40 @@
-# from pathlib import Path
-#
-# ALLOWED_FILE_TYPES = (
-#     ".mul",
-#     ".png",
-#     ".txt",
-#     ".Z_mtrx",
-#     ".flm",
-#     ".log",
-#     ".SM4",
-#     ".dat",
-#     ".sxm",
-#     ".vms",
-#     ".nid",
-#     ".jpg",
-#     ".jpeg",
-#     ".csv",
-# )
-
 import sys
 
 from PyQt6.QtWidgets import QApplication
 
 from prosurf.gui import MainGui
-from prosurf.html_rendering import create_html
-from prosurf.spm.mtrx import StmMatrix
-
-ProcessObject = StmMatrix
 
 
 def main():
-    # app = QApplication(sys.argv)
-    # window = MainGui()
-    # window.show()
-    # sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    window = MainGui()
+    window.show()
+    sys.exit(app.exec())
+
     # f = "/home/matthias/github/proespm-py3/tests/test_files/20201111--4_1.Z_mtrx"
-    f = "/Users/matthias/github/prosurf/tests/testdata/20201111--4_1.Z_mtrx"
-    m = StmMatrix(f)
-    m.process()
-    m.slide_num = 1
-    create_html([m],  "/Users/matthias/github/prosurf/",  "/Users/matthias/github/prosurf/tests/testdata")
+    # f = "/Users/matthias/github/prosurf/tests/testdata/20201111--4_1.Z_mtrx"
+    # m = StmMatrix(f)
+    # m.process()
+    # m.slide_num = 1
+    # create_html([m],  "/Users/matthias/github/prosurf/",  "/Users/matthias/github/prosurf/tests/testdata")
+    # files_dir = "/Users/matthias/Downloads/matrix_data/2024-07-10"
+    #
+    # file_lst: list[str] = []
+    # for entry in os.scandir(files_dir):
+    #     if entry.path.endswith(ALLOWED_FILE_TYPES) and entry.is_file():
+    #         file_lst.append(entry.path)
+    #
+    # slide_num = 1
+    # ms: list[ProcessObject] = []
+    # for f in file_lst:
+    #     if f.endswith("Z_mtrx"):
+    #         m = StmMatrix(f)
+    #         m.process()
+    #         m.slide_num = slide_num
+    #         slide_num += 1
+    #         ms.append(m)
+    #
+    # create_html(ms, files_dir, files_dir)
 
 
 if __name__ == "__main__":
