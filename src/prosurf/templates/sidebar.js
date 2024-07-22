742b16c55082bf6aaf11a4edcb8f2e787a9fcb37
matkrin <matkrin@protonmail.com>
2024-07-22 09:16:28 +0200

    Sidebar closes on item click

--- src/prosurf/templates/sidebar.js
+++ src/prosurf/templates/sidebar.js
@@ -2,7 +2,20 @@ const menu = document.getElementById("menu");
 const sidebar = document.getElementsByClassName("sidebar")[0];
 let isSidebarOpen = false
 
+/* Toggle the sidebar */
 menu.addEventListener("click", () => {
     sidebar.style.width = isSidebarOpen ? "0%" : "25%"
     isSidebarOpen = !isSidebarOpen
 })
+
+const sidebarItems = document
+    .getElementsByClassName("sidebar-content")[0]
+    .getElementsByTagName("li");
+
+/* Close the sidebar when one element is clicked */
+for (it of sidebarItems) {
+    it.addEventListener("click", () => {
+        sidebar.style.width = "0%"
+        isSidebarOpen = false
+    })
+}
