b3c395ae2f5be3f9062bda314dee853cb295093b
matkrin <matkrin@protonmail.com>
2025-11-15 10:42:29 +0100

    fix: Reading of NanoSurf .nid files

--- src/proespm/spm/nid.py
+++ src/proespm/spm/nid.py
@@ -3,15 +3,15 @@ from typing import Any, Hashable, Self, final
 
 import numpy as np
 from dateutil import parser
-from numpy._typing import NDArray
 
-from proespm.fileinfo import Fileinfo
 from proespm.config import Config
+from proespm.fileinfo import Fileinfo
 from proespm.labjournal import Labjournal
 from proespm.spm.spm import SpmImage
 
+
 FLOAT_REGEX = re.compile(r"[+-]?([0-9]*[.])?[0-9]+")
-UNITS_REGEX = re.compile(r"[a-zA-Zµ]+")
+UNITS_REGEX = re.compile(r"[a-zA-Zµ°]+")
 
 
 @final
@@ -23,72 +23,76 @@ class SpmNid:
         self.m_id = self.fileinfo.filename
         self.sheet_id: str | None = None
         self.slide_num: int | None = None
-        self.labjournal_data: dict[Hashable, Any] | None = None
+        self.labjournal_data: dict[Hashable, Any] | None = None  # pyright: ignore[reportExplicitAny]
 
         with open(filepath, "rb") as f:
             content = f.read()
 
         content_list = content.split(b"\r\n\r\n")
-        header = get_header(content_list)
+        header = _get_header(content_list)
 
-        file_meta = get_file_meta(content_list)
+        file_meta = _get_file_meta(content_list)
         self.op_mode = file_meta["Op. mode"]
 
-        self.xsize = read_float_from_string(file_meta["Image size"])
-        xsize_units = read_units_from_string(file_meta["Image size"])
+        self.xsize = _read_float_from_string(file_meta["Image size"])
+        xsize_units = _read_units_from_string(file_meta["Image size"])
         if xsize_units == "µm":
             self.xsize *= 1000
         self.ysize = self.xsize
 
         # scan_dir_up_down = file_meta["Scan direction"]
 
-        self.xoffset = read_float_from_string(file_meta["X-Pos"])
-        xoffset_units = read_units_from_string(file_meta["X-Pos"])
+        self.xoffset = _read_float_from_string(file_meta["X-Pos"])
+        xoffset_units = _read_units_from_string(file_meta["X-Pos"])
         if xoffset_units == "µm":
             self.xoffset *= 1000
 
-        self.yoffset = read_float_from_string(file_meta["Y-Pos"])
-        y_offset_units = read_units_from_string(file_meta["Y-Pos"])
+        self.yoffset = _read_float_from_string(file_meta["Y-Pos"])
+        y_offset_units = _read_units_from_string(file_meta["Y-Pos"])
         if y_offset_units == "µm":
             self.yoffset *= 1000
 
-        self.tilt = read_float_from_string(file_meta["Rotation"])
-        self.line_time = read_float_from_string(file_meta["Time/Line"])
+        self.tilt = _read_float_from_string(file_meta["Rotation"])
+        self.line_time = _read_float_from_string(file_meta["Time/Line"])
         self.scan_duration = (
-            self.line_time * read_float_from_string(file_meta["Lines"]) / 1000
+            self.line_time * _read_float_from_string(file_meta["Lines"]) / 1000
         )
 
         self.datetime = parser.parse(
             file_meta["Date"] + " " + file_meta["Time"]
         )
-        self.bias = read_float_from_string(file_meta["Tip voltage"])
+        self.bias = _read_float_from_string(file_meta["Tip voltage"])
         # current; or setpoint AFM in %
-        self.current = read_float_from_string(file_meta["Setpoint"])
-        self.p_gain = read_float_from_string(file_meta["P-Gain"])
-        self.i_gain = read_float_from_string(file_meta["I-Gain"])
+        self.current = _read_float_from_string(file_meta["Setpoint"])
+        self.p_gain = _read_float_from_string(file_meta["P-Gain"])
+        self.i_gain = _read_float_from_string(file_meta["I-Gain"])
 
         channels = get_channels(header)
-        channel_meta_list = get_channels_meta(content_list, channels)
+        channel_meta_list = _get_channels_meta(content_list, channels)
         self.xres = int(channel_meta_list[0]["Points"])
         self.yres = int(channel_meta_list[0]["Lines"])
 
         # `Z-Axis` for topo AFM/STM, `Tip Current` for current STM, `Amplitude` for AFM
         # scantype = channel_meta_list[0]["Dim2Name"]
-        datatype = (
-            np.int16 if channel_meta_list[0]["SaveBits"] == "16" else np.int32
-        )
+        save_bits = int(channel_meta_list[0]["SaveBits"])
+        datatype = np.int16 if save_bits == 16 else np.int32
 
         for channel in channel_meta_list:
             assert self.xres == int(channel["Points"])
             assert self.yres == int(channel["Lines"])
 
-        img_data_block = get_img_data_block(content_list)
-        img_data_list = read_img_data(img_data_block, datatype, 4)  # pyright: ignore[reportArgumentType]
+        num_channels = len(channel_meta_list)
+        bytes_to_read = (
+            int(self.xres * self.yres * save_bits / 8) * num_channels
+        )
+        img_data_start = len(content) - bytes_to_read
+        img_data = np.frombuffer(content[img_data_start:], dtype=datatype)
+        img_data_list = np.array_split(img_data, num_channels)
 
         fw_idx = None
         bw_idx = None
         for i, ch in enumerate(channel_meta_list):
-            if ch["Dim2Name"] == "Z-Axis":
+            if ch["Dim2Name"] in ["Z-Axis", "Topography"]:
                 if ch["Frame"] == "Scan forward":
                     fw_idx = i
                 elif ch["Frame"] == "Scan backward":
@@ -139,7 +143,7 @@ class SpmNid:
             self.sheet_id, self.labjournal_data = metadata
 
 
-def get_header(content_list: list[bytes]) -> list[bytes]:
+def _get_header(content_list: list[bytes]) -> list[bytes]:
     return content_list[0].split(b"\r\n")
 
 
@@ -152,7 +156,7 @@ def get_channels(header: list[bytes]) -> list[bytes]:
     return channels
 
 
-def get_file_meta(content_list: list[bytes]) -> dict[str, str]:
+def _get_file_meta(content_list: list[bytes]) -> dict[str, str]:
     file_meta: dict[str, str] = {}
     for block in content_list:
         if block.startswith(b"[DataSet-Info"):
@@ -165,7 +169,7 @@ def get_file_meta(content_list: list[bytes]) -> dict[str, str]:
     return file_meta
 
 
-def get_channels_meta(
+def _get_channels_meta(
     content_list: list[bytes], channels: list[bytes]
 ) -> list[dict[str, str]]:
     channels_meta_list: list[dict[str, str]] = []
@@ -186,23 +190,9 @@ def get_channels_meta(
     return channels_meta_list
 
 
-def get_img_data_block(content_list: list[bytes]) -> bytes:
-    data_block = content_list[-1]
-    # first 4 bytes are some identifier
-    assert data_block[:4] == b"\r\n#!"
-    return data_block[4:]
-
-
-def read_img_data(
-    img_data_block: bytes, dtype: np.int16 | np.int32, num_channels: int
-) -> list[NDArray[np.int16 | np.int32]]:
-    img_data = np.frombuffer(img_data_block, dtype=dtype)
-    return np.array_split(img_data, num_channels)
-
-
-def read_float_from_string(text: str) -> float:
+def _read_float_from_string(text: str) -> float:
     return float(FLOAT_REGEX.match(text).group(0))  # pyright: ignore[reportOptionalMemberAccess]
 
 
-def read_units_from_string(text: str) -> str:
+def _read_units_from_string(text: str) -> str:
     return UNITS_REGEX.findall(text)[0]  # pyright: ignore[reportAny]
