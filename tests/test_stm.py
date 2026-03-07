1d0872d1003bd0a4d64133083c6943302cd07240
matkrin <matkrin@protonmail.com>
2026-03-07 08:37:48 +0100

    test: More STM tests

--- tests/test_stm.py
+++ tests/test_stm.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from proespm.spm.nid import SpmNid
+from proespm.spm.mtrx import StmMatrix
+from proespm.spm.sm4 import StmSm4
 
 testdata = Path(__file__).parent / "testdata"
 
@@ -7,7 +9,7 @@ STM_FLM = testdata / "stm-aarhus-flm.flm"
 STM_MUL_A = testdata / "stm-aarhus-mul-a.mul"
 STM_MUL_B = testdata / "stm-aarhus-mul-b.mul"
 STM_MATRIX = testdata / "20201111--4_1.Z_mtrx"
-STM_RHK = testdata / "stm-rhk-sm4.SM4"
+STM_RHK = testdata / "data0740.SM4"
 STM_NID = testdata / "stm-nanosurf-nid.nid"
 AFM_NID = testdata / "afm-nanosurf-nid.nid"
 
@@ -40,3 +42,26 @@ def test_afm_nid():
     assert nid.line_time == 498.0
     assert nid.xoffset == 574.0
     assert nid.yoffset == -1200.0
+
+
+def test_stm_matrix():
+    mtrx = StmMatrix(str(STM_MATRIX))
+    assert mtrx.current == 0.30
+    assert round(mtrx.bias, 2) == 0.60
+    assert mtrx.xsize == 100.00
+    assert mtrx.ysize == 100.00
+    assert mtrx.xres == 400
+    assert mtrx.yres == 400
+    assert mtrx.line_time == 200.00
+    assert mtrx.scan_duration == 80.00
+
+
+def test_stm_sm4():
+    mtrx = StmSm4(str(STM_RHK))
+    assert round(mtrx.current, 2) == 1.00
+    assert round(mtrx.bias, 2) == 0.51
+    assert round(mtrx.xsize, 2) == 80.00
+    assert round(mtrx.ysize, 2) == 80.00
+    assert mtrx.xres == 512
+    assert mtrx.yres == 512
+    assert round(mtrx.line_time) == 50.00
